c9d8042f76a4a7608ff74b0fe102a27b549c1472
Burenkovuser <nb2207@yandex.ru>
2017-01-08 17:53:36 +0600

    Student

---
+++ DZ14Strings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DZ14Strings
+//
+//  Created by Vasilii on 08.01.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
